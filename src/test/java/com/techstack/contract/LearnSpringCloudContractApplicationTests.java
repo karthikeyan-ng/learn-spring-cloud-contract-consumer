e38b61ec769277f745596b70ca449449140a53b6
Karthikeyan Nithiyanandam <rabodevops17@RABODEVOPS17s-MacBook-Pro.local>
2020-03-19 22:49:27 +0530

    Initial commit

---
+++ src/test/java/com/techstack/contract/LearnSpringCloudContractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techstack.contract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringCloudContractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
