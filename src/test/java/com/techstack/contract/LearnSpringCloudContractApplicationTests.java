5208014debfcb4bd2117a938b04fdf07b92d08a6
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-03-19 23:39:04 +0530

    close #2: Created a WireMock stub for the given API and verify the results.

--- src/test/java/com/techstack/contract/LearnSpringCloudContractApplicationTests.java
+++ src/test/java/com/techstack/contract/LearnSpringCloudContractApplicationTests.java
@@ -1,11 +1,15 @@
 package com.techstack.contract;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.apache.http.impl.conn.Wire;
 import org.assertj.core.api.BDDAssertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+@AutoConfigureWireMock(port = 8081)
 @SpringBootTest
 class LearnSpringCloudContractApplicationTests {
 
@@ -15,12 +19,16 @@ class LearnSpringCloudContractApplicationTests {
 
 	@Test
 	public void test_should_return_all_employee_name() {
-		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8081/employees", String.class);
 
 		String json = "[\"Pascal\", \"Thomas\"]";
 
-		BDDAssertions.then(response.getStatusCode()).isEqualTo(201);
+		WireMock.stubFor(WireMock.get(WireMock.urlEqualTo("/employees"))
+				.willReturn(WireMock.aResponse().withBody(json).withStatus(201)));
+
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8081/employees", String.class);
+
+		BDDAssertions.then(response.getStatusCode().value()).isEqualTo(201);
 		BDDAssertions.then(response.getBody()).isEqualTo(json);
 	}
 }
