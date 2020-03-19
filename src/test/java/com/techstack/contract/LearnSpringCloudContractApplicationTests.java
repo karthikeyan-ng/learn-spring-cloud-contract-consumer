7fa1b55bc29b04683a03b6daec58b504706458fa
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-03-19 23:20:38 +0530

    close #1: Created a Simple testcase to call employees service and get employee names.

--- src/test/java/com/techstack/contract/LearnSpringCloudContractApplicationTests.java
+++ src/test/java/com/techstack/contract/LearnSpringCloudContractApplicationTests.java
@@ -1,7 +1,10 @@
 package com.techstack.contract;
 
+import org.assertj.core.api.BDDAssertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 class LearnSpringCloudContractApplicationTests {
@@ -10,4 +13,14 @@ class LearnSpringCloudContractApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	public void test_should_return_all_employee_name() {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8081/employees", String.class);
+
+		String json = "[\"Pascal\", \"Thomas\"]";
+
+		BDDAssertions.then(response.getStatusCode()).isEqualTo(201);
+		BDDAssertions.then(response.getBody()).isEqualTo(json);
+	}
 }
